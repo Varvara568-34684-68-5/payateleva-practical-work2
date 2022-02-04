038f120d4214f3dcbd6ef111ae436aed38548df2
Студент ТУЗ <Student@MyKMK.kulmk.ru>
2022-02-04 11:42:45 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShashkinaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите цифру");
+            int a = 17;
+
+            Console.WriteLine(a);
+
+        }
+    }
+}
